4c2ff128974835930595d2b4307e4a9f1940e9ef
Vladyslav Permiakov <vlad.piermiakov@gmail.com>
2022-12-26 01:10:18 +0200

    Remove unnessesary using and optimize code in LoadList.cs

--- CourseworkWinForms/LoadList.cs
+++ CourseworkWinForms/LoadList.cs
@@ -6,6 +6,21 @@ namespace CourseworkWinForms
 {
     public partial class Form1
     {
+        public static void checkLnk(List<Applications> result, DirectoryInfo directoryDataInfo)
+        {
+            foreach (FileInfo file in directoryDataInfo.GetFiles()) //проходим по файлам
+            {
+                //получаем расширение файла и проверяем подходит ли оно нам 
+                if (Path.GetExtension(file.Name) == ".lnk")
+                {
+                    Applications testApp = new Applications();
+                    testApp.name = file.Name;
+                    testApp.path = file.DirectoryName;
+                    result.Add(testApp);
+                }
+            }
+        }
+
         public static List<Applications> GetApplications()
         {
             List<Applications> result = new List<Applications>();
@@ -14,60 +29,20 @@ namespace CourseworkWinForms
             string pathUser = "C:\\Users\\" + userName + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs";
 
             DirectoryInfo directoryProgramDataInfo = new DirectoryInfo(pathProgramData);
-            foreach (var file in directoryProgramDataInfo.GetFiles()) //проходим по файлам
-            {
-                //получаем расширение файла и проверяем подходит ли оно нам 
-                if (Path.GetExtension(file.Name) == ".lnk")
-                {
-                    Applications testApp = new Applications();
-                    testApp.name = file.Name;
-                    testApp.path = file.DirectoryName;
-                    result.Add(testApp);
-                }
-            }
+            checkLnk(result, directoryProgramDataInfo);
             string[] programDataDirectories = Directory.GetDirectories(pathProgramData);
             for (int i = 0; i < programDataDirectories.Length; i++)
             {
                 DirectoryInfo newDirectoryInfo = new DirectoryInfo(programDataDirectories[i]);
-                foreach (var file in newDirectoryInfo.GetFiles()) //проходим по файлам
-                {
-                    //получаем расширение файла и проверяем подходит ли оно нам 
-                    if (Path.GetExtension(file.Name) == ".lnk")
-                    {
-                        Applications testApp = new Applications();
-                        testApp.name = file.Name;
-                        testApp.path = file.DirectoryName;
-                        result.Add(testApp);
-                    }
-                }
+                checkLnk(result, newDirectoryInfo);
             }
             DirectoryInfo directoryUserInfo = new DirectoryInfo(pathUser);
-            foreach (var file in directoryUserInfo.GetFiles()) //проходим по файлам
-            {
-                //получаем расширение файла и проверяем подходит ли оно нам 
-                if (Path.GetExtension(file.Name) == ".lnk")
-                {
-                    Applications testApp = new Applications();
-                    testApp.name = file.Name;
-                    testApp.path = file.DirectoryName;
-                    result.Add(testApp);
-                }
-            }
+            checkLnk(result, directoryUserInfo);
             string[] userDirectories = Directory.GetDirectories(pathUser);
             for (int i = 0; i < userDirectories.Length; i++)
             {
                 DirectoryInfo newDirectoryInfo = new DirectoryInfo(userDirectories[i]);
-                foreach (var file in newDirectoryInfo.GetFiles()) //проходим по файлам
-                {
-                    //получаем расширение файла и проверяем подходит ли оно нам 
-                    if (Path.GetExtension(file.Name) == ".lnk")
-                    {
-                        Applications testApp = new Applications();
-                        testApp.name = file.Name;
-                        testApp.path = file.DirectoryName;
-                        result.Add(testApp);
-                    }
-                }
+                checkLnk(result, newDirectoryInfo);
             }
 
             return result;
